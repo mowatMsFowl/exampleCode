934807a9ca979daac2b5de2bd9b7e894a586b8df
mowatMsFowl <36801087+mowatMsFowl@users.noreply.github.com>
2024-05-17 09:52:56 -0400

    Add files via upload

---
+++ DisplayImage.java
@@ -0,0 +1,35 @@
+import java.awt.Image;
+import java.awt.BorderLayout;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import javax.swing.*;
+
+class DisplayImage
+{
+   public static void main(String[] args)
+   {
+      Image image = null;
+      try {
+        // Read from a file
+         File file = new File("bubble.jpg");
+         image = ImageIO.read(file);
+      // 
+//         // Read from an input stream
+//          InputStream is = new BufferedInputStream(new FileInputStream("bubble.jpg"));
+//          image = ImageIO.read(is);
+//       
+//         // Read from a URL
+//          URL url = new URL("http://hostname.com/image.gif");
+//          image = ImageIO.read(url);
+      } catch (IOException e) {
+      }
+    
+    // Use a label to display the image
+      JFrame frame = new JFrame();
+      JLabel label = new JLabel(new ImageIcon(image));
+      frame.getContentPane().add(label, BorderLayout.CENTER);
+      frame.pack();
+      frame.setVisible(true);
+   }
+}
